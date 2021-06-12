42f114e31b87c783d141806390f60eb75c30eb03
satishru <ujjammanavar.satish@gmail.com>
2021-06-12 18:37:47 +0530

    Added comment

--- app/src/main/java/app/android/speedtest/MainActivity.java
+++ app/src/main/java/app/android/speedtest/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        ////
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
